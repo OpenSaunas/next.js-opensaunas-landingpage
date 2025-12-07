d25326949622c22155bc37871cbde31765df72b0
dannysmson <dannysmson@naver.com>
2025-12-07 16:12:42 +0900

    [Fix]: 이미지 수정

--- components/sections/IllustSection.tsx
+++ components/sections/IllustSection.tsx
@@ -4,7 +4,22 @@ const IllustSection = () => {
   return (
     <section className="w-full">
       {/* Image */}
-      <Image src="/img_illust.png" alt="Illust Image" width={1440} height={909} className="w-full h-auto" priority />
+      <Image
+        src="/img_illust.png"
+        alt="Illust Image"
+        width={1440}
+        height={909}
+        className="w-full h-auto hidden min-[1080px]:block"
+        priority
+      />
+      <Image
+        src="/img_illust_mobile.png"
+        alt="Illust Mobile Image"
+        width={1080}
+        height={1500}
+        className="w-full h-auto block min-[1080px]:hidden"
+        priority
+      />
     </section>
   );
 };
